45bdd200f3f02dc2936f563746525dc7c435e490
c100bit <c100bit@gmail.com>
2021-12-26 02:06:17 +0300

    added env interval and description

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,19 +1,30 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 import { BitrixService } from 'src/bitrix/bitrix.service';
 import { MailService } from 'src/mail/mail.service';
 
 @Injectable()
-export class TasksService {
+export class TasksService implements OnModuleInit {
   private readonly logger = new Logger(TasksService.name);
 
   constructor(
     private mailService: MailService,
     private bxService: BitrixService,
+    private schedulerRegistry: SchedulerRegistry,
+    private config: ConfigService,
   ) {}
 
-  @Cron(CronExpression.EVERY_MINUTE)
-  async handleCron() {
+  onModuleInit() {
+    const ms = 60000;
+    const interval = setInterval(
+      async () => await this.handleInterval(),
+      this.config.get<number>('INTERVAL_MIN') * ms,
+    );
+    this.schedulerRegistry.addInterval('appInterval', interval);
+  }
+
+  async handleInterval() {
     this.logger.log('Task started');
     const items = await this.mailService.perform();
     this.bxService.addListElements(items);
